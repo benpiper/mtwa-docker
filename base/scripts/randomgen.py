b7f1b6b02aa2015546174b55003c76152888bf2d
Ben Piper <ben@benpiper.com>
2017-11-10 15:13:52 -0500

    call function

--- base/scripts/randomgen.py
+++ base/scripts/randomgen.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python
 import string, random
+import cgitb
+
+cgitb.enable()
 
 def random_generator(size=10485760, chars=string.ascii_uppercase):
  print ''.join(random.choice(chars) for _ in range(size))
+
+random_generator()
