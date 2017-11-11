80a3c38d3d8170b6fdce482d09aa55b41419022a
Ben Piper <ben@benpiper.com>
2017-11-10 22:14:47 -0500

    what?!

--- base/scripts/randomgen.py
+++ base/scripts/randomgen.py
@@ -4,9 +4,9 @@ import socket
 
 print '''
 '''
+print socket.gethostname()
 
 def random_generator(size=15372000, chars=string.printable):
  print ''.join(random.choice(chars) for _ in range(size))
 
-print socket.gethostname()
 random_generator()
