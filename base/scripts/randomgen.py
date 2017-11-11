e276b7bf7edab4b2a3f1e06878107b99504f4097
Ben Piper <ben@benpiper.com>
2017-11-11 00:38:59 -0500

    add wait

--- base/scripts/randomgen.py
+++ base/scripts/randomgen.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python
 import string, random
 import socket
+import time
 
 print '''
+Waiting for 15 seconds...
 '''
+
+time.sleep(15)
 
 def random_generator(size=15334400, chars=string.printable):
  return ''.join(random.choice(chars) for _ in range(size))
