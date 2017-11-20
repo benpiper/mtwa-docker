89a939ce6b2e7216ea8e70d22dc502812de26bae
Ben Piper <ben@benpiper.com>
2017-11-20 16:45:52 -0500

    after pause, print hostname followed by space

--- base/scripts/randomgen.py
+++ base/scripts/randomgen.py
@@ -3,8 +3,6 @@ import string, random
 import socket
 import time
 
-print socket.gethostname()
-
 print '''
 Waiting for 15 seconds...
 '''
@@ -14,4 +12,4 @@ time.sleep(15)
 def random_generator(size=2621440, chars=string.printable):
  return ''.join(random.choice(chars) for _ in range(size))
 
-print random_generator()
+print socket.gethostname()+' '+random_generator()
