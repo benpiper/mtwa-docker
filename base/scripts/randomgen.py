b647ae6d0e3722600f6a658f6a26554a77dc85de
Ben Piper <ben@benpiper.com>
2017-11-20 17:39:03 -0500

    reduce to 100KB

--- base/scripts/randomgen.py
+++ base/scripts/randomgen.py
@@ -9,7 +9,7 @@ Waiting for 15 seconds...
 
 time.sleep(15)
 
-def random_generator(size=1024000, chars=string.printable):
+def random_generator(size=102400, chars=string.printable):
  return ''.join(random.choice(chars) for _ in range(size))
 
 print socket.gethostname() + '  ' + random_generator()
