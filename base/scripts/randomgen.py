9d4441a157a554da010630f0da1da63f97040467
Ben Piper <ben@benpiper.com>
2017-11-21 00:59:30 -0500

    print hostname at end

--- base/scripts/randomgen.py
+++ base/scripts/randomgen.py
@@ -10,4 +10,4 @@ Random data generator
 def random_generator(size=6291456, chars=string.printable):
  return ''.join(random.choice(chars) for _ in range(size))
 
-print socket.gethostname() + '  ' + random_generator()
+print socket.gethostname() + '  ' + random_generator() + '  ' + socket.gethostname()
