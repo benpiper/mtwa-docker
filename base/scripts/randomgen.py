cf3a2a45a32d81aef9a5220682301c666ba15c6f
Ben Piper <ben@benpiper.com>
2017-11-20 23:39:43 -0500

    increase to 12MB

--- base/scripts/randomgen.py
+++ base/scripts/randomgen.py
@@ -7,7 +7,7 @@ print '''
 Random data generator
 '''
 
-def random_generator(size=102400, chars=string.printable):
+def random_generator(size=12582912, chars=string.printable):
  return ''.join(random.choice(chars) for _ in range(size))
 
 print socket.gethostname() + '  ' + random_generator()
